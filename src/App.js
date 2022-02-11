32c03f8dbe735fc83e4698e0f5df5926006cf183
varsharanitekale <varsharanitekale50@gmail.com>
2022-02-11 22:04:27 +0530

    add code

--- src/App.js
+++ src/App.js
@@ -3,23 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      mycards:[{
+        id:1,
+        title:'What is your name?',
+        description:"my name full name is Varsharani Maroti Tekale.",
+        buttonText:"ok"
+      },
+      {
+        id:2,
+        title:'What is your name?',
+        description:"my name full name is Varsharani Maroti Tekale.",
+        buttonText:"ok"
+      },
+      {
+        id:3,
+        title:'What is your name?',
+        description:"my name full name is Varsharani Maroti Tekale.",
+        buttonText:"ok"
+      },
+      {
+        id:4,
+        title:'What is your name?',
+        description:"my name full name is Varsharani Maroti Tekale.",
+        buttonText:"ok"
+      },
+      {
+        id:5,
+        title:'What is your name?',
+        description:"my name full name is Varsharani Maroti Tekale.",
+        buttonText:"ok"
+      },]
+    }
+  }
+
   render() {
+    
+    console.log('mycards',this.state.mycards);
+    const Items=this.state.mycards.map(item=>(
+      <div className='card'>
+       
+        
+        <div className='cardHeader'>{item.title}</div>
+        <hr/>
+        <div className='cardBody'>{item.description}</div>
+        <div className='cardFooter'><button>{item.buttonText}</button></div>
+
+      </div>
+    ))
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  {Items}
+
+
+
       </div>
     );
   }
